699de828edd7660bb436c54812df880f74b370b9
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2025-12-17 12:11:46 +0200

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,19 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
 config.use_fancy_tab_bar = true
+
+config.font = wezterm.font("FiraCode Nerd Font")
+
+config.keys = {
+	{
+		key = "H",
+		mods = "CTRL|SHIFT",
+		action = act.ActivateCopyMode,
+	},
+}
 
 return config
