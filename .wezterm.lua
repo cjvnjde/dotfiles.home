7bc815e3a3262c889211ca0d5e577cccfc1b3f39
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2025-12-17 13:08:43 +0200

    chore(wezterm): disable fancy tab bar and hide for single tabs
    Simplify tab bar UI by disabling fancy mode and auto-hiding when only one tab is open for a cleaner interface.

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,15 +4,27 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
-config.use_fancy_tab_bar = true
+config.use_fancy_tab_bar = false
 
 config.font = wezterm.font("FiraCode Nerd Font")
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = "NeverPrompt"
 
 config.keys = {
 	{
 		key = "H",
 		mods = "CTRL|SHIFT",
 		action = act.ActivateCopyMode,
+	},
+	{
+		key = "t",
+		mods = "CMD",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = act.CloseCurrentPane({ confirm = false }),
 	},
 }
 
